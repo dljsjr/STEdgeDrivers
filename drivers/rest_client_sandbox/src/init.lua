a9a0f0e275d10755f1ea1772958a34c293b01c75
Doug Stephen <dljs.jr@dougstephenjr.com>
2023-09-22 15:48:31 -0500

    SSL self-signed cert test.

--- drivers/rest_client_sandbox/src/init.lua
+++ drivers/rest_client_sandbox/src/init.lua
@@ -2,11 +2,37 @@ local Driver = require "st.driver"
 
 local log = require "log"
 local socket = require "cosock.socket"
+local ssl = require "cosock.ssl"
+local st_utils = require "st.utils"
+
+local SSL_CONFIG = {
+  mode = "client",
+  protocol = "any",
+  verify = "peer",
+  options = "all",
+  cafile="cert.pem"
+}
+
+--- @param ip string
+--- @param port number
+--- @return table
+local function make_ssl_socket(ip, port)
+  local sock = assert(socket.tcp())
+  assert(sock:settimeout(10))
+  assert(sock:connect(ip, port))
+  assert(sock:setoption("keepalive", true))
+  sock = assert(ssl.wrap(sock, SSL_CONFIG))
+  assert(sock:dohandshake())
+  return sock
+end
 
 function disco(_, _, should_continue)
   log.info('beginning discovery')
+  local sock = make_ssl_socket("192.168.1.132", 3030)
   while should_continue() do
-    socket.sleep(0.2)
+    local recv = assert(sock:receive())
+    log.info(st_utils.stringify_table(recv, "Socket Receive", true))
+    socket.sleep(1.0)
   end
   log.info('ending discovery')
 end
